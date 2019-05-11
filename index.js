9c4e6d82e9b86ce06c7c381d5ec3841c0f5c38fb
ferryrules <ferris@ferryrules.com>
2019-05-11 17:01:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+  const cards = []
+  for (var i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return cards
+}
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--
+    }
+}
